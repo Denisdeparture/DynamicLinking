67cd58a1b43a030a93b7f3c0f5a588d32372e2e4
user <user@WIN-Q0NS67721NA>
2024-01-18 17:07:21 +0300

    Добавьте файлы проекта.

---
+++ Lib/ClassWork.cs
@@ -0,0 +1,15 @@
+﻿namespace Lib
+{
+    public class ClassWork
+    {
+        private static string BaseText = "Hello World";
+        static void Show(string? text) => Console.WriteLine(text ?? BaseText);
+        static void Reverse(string text) 
+        {
+            string t = "";
+            for (int i = 0; i < text.Length; i++) t += text[text.Length - i - 1];
+            Console.WriteLine(t); 
+        }
+       
+    }
+}
---
+++ TestAssembly/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Threading.Channels;
+class Work
+{
+    static void Main()
+    {
+        Assembly asm = Assembly.LoadFrom("Lib.dll");
+        Type[] types = asm.GetTypes();
+        MethodInfo? objs;
+        List<string> nameMethod = new List<string>()
+        {
+            "Show","Reverse"
+        };
+        foreach (Type t in types)
+        {
+            for (int counterMethod = 0; counterMethod < nameMethod.Count; counterMethod++)
+            {
+                objs = t.GetMethod(nameMethod[counterMethod], BindingFlags.NonPublic | BindingFlags.Static);
+                if (objs != null)
+                {
+                    objs?.Invoke(null, new object[] { "World" });
+                }
+            }
+        }
+    }
+}
